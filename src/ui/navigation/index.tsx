14c6a64dfe71b0fdd635bd033f3c7c5320546b65
Hunter <hhunterr69@gmail.com>
2023-10-18 09:49:08 +0530

    chore: add absolute import , code cleanup

--- src/ui/navigation/index.tsx
+++ src/ui/navigation/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
+import {Text} from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import * as eva from '@eva-design/eva';
 import {ApplicationProvider} from '@ui-kitten/components';
-import {default as theme} from '../../../theme.json'; // <-- Import app theme
+import {default as theme} from 'theme.json'; // <-- Import app theme
 
-import {AuthenticationStack} from './AuthenticationStack';
-
-import {RootStackParamList} from '../../types';
-import {Splash} from '../screens/splash';
+import {RootStackParamList} from '@src/types';
 
 // Global Constants
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -17,12 +15,7 @@ export const RootNavigator: React.FC = () => {
   return (
     <ApplicationProvider {...eva} theme={{...theme}}>
       <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Splash"
-          screenOptions={{headerShown: false}}>
-          <Stack.Screen name="Splash" component={Splash} />
-          <Stack.Screen name="Auth" component={AuthenticationStack} />
-        </Stack.Navigator>
+        <Text>Calc App</Text>
       </NavigationContainer>
     </ApplicationProvider>
   );
